d85a5868a2505c5bc85bd1f10a91f5386fa6362f
HauseOwner <requipped0@gmail.com>
2026-05-27 20:12:53 -0600

    redesigned home page :D

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function show(id, btn) {
     document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
     document.querySelectorAll('.nav-btn').forEach(b => b.classList.remove('active'));
-    document.getElementById('page-' + id).classList.add('active');
+    document.getElementById('pg-' + id).classList.add('active');
     btn.classList.add('active');
 }
